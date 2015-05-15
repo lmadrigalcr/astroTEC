1439419a313753f92a0b5d31f3d97c0012414e22
José Andrés García <jags9415@gmail.com>
2015-05-14 21:06:01 -0600

    Create fill script

---
+++ sql/fill.sql
@@ -0,0 +1,10 @@
+USE `astroDB` ;
+
+INSERT INTO EstadoUsuario (idEstado, estado) VALUES
+(1, 'activo'),
+(2, 'inactivo'),
+(3, 'bloqueado');
+
+INSERT INTO Usuarios (nombre, apellido1, apellido2, genero, correo, password, fk_idEstado) VALUES
+('Administrador', NULL, NULL, 'M', 'admin@example.com', SHA2('123', 256), 1),
+('Jhon', 'Doe', NULL, 'M', 'jhondoe@example.com', SHA2('abc', 256), 1);
