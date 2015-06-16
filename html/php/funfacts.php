bc49605490ae841c91da1b383d94a8b6810470bf
José Andrés García <jags9415@gmail.com>
2015-06-15 18:34:14 -0600

    Update funfacts

--- html/php/funfacts.php
+++ html/php/funfacts.php
@@ -1,27 +1,26 @@
 <?php
 require_once('db.php');
 
-$facts = array();
+$facts = array(); 
 
 function getFunFacts()
 {
 	global $facts, $conn;
-	$sql = "SELECT contenido, DATE_FORMAT(fecha, '%Y') AS year, DATE_FORMAT(fecha, '%M') AS month, DATE_FORMAT(fecha, '%d') AS day, DATE_FORMAT(fecha, '%W') AS dayName,  contenido FROM datoscuriosos";
+	$sql = "SELECT contenido 
+	        FROM DatosCuriosos
+					ORDER BY RAND() LIMIT 3";
 
 	$result = $conn->query($sql);
 
-	if($result)
-	{
 		if($result->num_rows > 0)
 		{
-			$i = 0;
 			while($row = $result->fetch_assoc())
 			{
 				echo 
-				"<div class='col-md-4'>
+				"<div class='col-md-4 funfact'>
 					<div class='panel panel-default'>
 						<div class='panel-heading'>
-							<h1 class='panel-title'>$row[day]-$row[month]-$row[year]</h1>
+							<h1 class='panel-title'></h1>
 						</div>
 						<div class='panel-body'>
 							<p>
@@ -31,12 +30,5 @@ function getFunFacts()
 					</div>
 				</div>";
 			}
-
-			$result->close();
 		}
-		else
-		{
-
-		}
-	}
 }
