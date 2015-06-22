732f18e4cc3eb9754d5390958a3df447eec1b8fd
Leonardo Madrigal <lmadrigal09@gmail.com>
2015-06-21 21:22:31 -0600

    admin update

--- html/adminPage.php
+++ html/adminPage.php
@@ -341,7 +341,7 @@
 		              <div class="container">
 							<div class="col-md-10">
 							    <div class="form-area">
-							    	<form action="" method="post" enctype="multipart/form-data">  
+							    	<form id="frmUpdateGalleryUpload" action="" method="post" enctype="multipart/form-data">  
 					                    <h3 style="margin-bottom: 25px; text-align: center;">Modificar</h3>
 					    				<div class="form-group">
 					    				<label> Publicación:</label>
@@ -367,7 +367,7 @@
 									 		</div>
 									 	<button type="button" onclick="addMoreModifyImages()">+</button>
 								 		</div>
-							        	<button type="button" class="btn btn-default" onclick="">Modificar</button>
+							        	<button id="modifyGalleryButton" type="button" class="btn btn-default" onclick="">Modificar</button>
 							        </form>
 							    </div>
 							</div>			         
